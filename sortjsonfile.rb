1c3761740656e454a67614f9102d0d5c8a901aa5
Saurabh-Singh5 <saurabh.singh5@ibm.com>
2022-11-08 00:46:57 +0000

    formula update

--- sortjsonfile.rb
+++ sortjsonfile.rb
@@ -2,12 +2,13 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sortjsonfile < Formula
+  include Language::Python::Virtualenv
   desc ""
   homepage ""
   url "https://github.com/singhsaurabh/sortjsonfile/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "d14a55861586e05c9acdbf1a8801810d318539728f8d8748e982b12322b10709"
 
   def install
-    bin.install "sortjsonfile"
+    virtualenv_install_with_resources
   end
 end
